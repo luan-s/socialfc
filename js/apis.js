1d99bf1785e1a392a50b0875aa4a1d5dbe4414be
luan-s <luanjs.if@gmail.com>
2015-11-25 19:35:15 -0200

    socialfc

---
+++ js/apis.js
@@ -0,0 +1,58 @@
+
+
+function statusChangeCallback(response) {
+  console.log('statusChangeCallback');
+  console.log(response);
+  if (response.status === 'connected') {
+    $(".login").hide();
+    testAPI();
+  } else if (response.status === 'not_authorized') {
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into this app.';
+  } else {
+    document.getElementById('status').innerHTML = 'Please log ' +
+      'into Facebook.';
+  }
+}
+
+(function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1189074134470014',
+    cookie     : true,  // enable cookies to allow the server to access 
+              // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.5' // use version 2.2
+
+
+  });
+
+  FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+  });
+}
+
+
+
+function testAPI() {
+  console.log('Welcome!  Fetching your information.... ');
+  FB.api('/me', function(response) {
+    console.log('Successful login for: ' + response.name);
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!';
+  });
+}
+
+$('#login').click(function(){
+  console.log("oi")
+  FB.login(function(response){
+    statusChangeCallback(response);
+  });
+});
--- js/script.js
+++ js/script.js
@@ -1,15 +1,47 @@
+var url = 'http://globoesporte.globo.com/servico/semantica/editorias/plantao/futebol/times/corinthians/feed.rss';
 var corTime;
+
+
+/*
+$(function () {
+	var urlRss = url;
+	$.ajax({
+		type: "GET",
+		url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(urlRss),
+		dataType: 'json',
+		error: function (xhr) {
+			var erro = xhr.responseText;
+			console.log('Erro ao ler o feed: ' + erro);
+		},
+		success: function (xml) {
+			values = xml.responseData.feed.entries;
+			console.log(JSON.stringify(values));
+		}
+	});
+});*/
 
 /*var i = 0;
 setInterval(function(){
 	$(".time > input").attr("placeholder","Ex: "+times[i++]);
 	if(i==times.length)i = 0;
-}, 3000);*/
+}, 3000);
+
+
+var feed = new Instafeed ({ 
+    get :  'tagged' , 
+    tagName :  'awesome' , 
+    clientId : '07fb82d2fea84e99bbe6bf0d1b3c7603',
+}); 
+feed.run(); 
+
+*/
 
 times = ["Atletico Mineiro","Avai","Chapecoense","Corinthians","Coritiba","Cruzeiro","Figueirense","Flamengo","Fluminense","Goiais","Gremio","Internacional","joinville","Palmeiras","Santos","Sao Paulo","Sport", "Vasco"]
 $(".nav-pills > li").click(function(){
 	$(".nav-pills > li").removeClass("active");
 	$(this).addClass("active");
+	$('.so-pane').addClass("hidden");
+	$('.'+$(this).attr('data-s')).removeClass("hidden");
 });	
 
 $("#ir").click(function(){
@@ -17,10 +49,3 @@ $("#ir").click(function(){
 	$('.pesquisa').css("padding","0");
 	$('.social').css("display","block");
 });
-
-var feed = new Instafeed ({ 
-    get :  'tagged' , 
-    tagName :  'awesome' , 
-    clientId : '9ee4e17658b245cca93856ac7efeb9cf',
-}); 
-feed.run(); 
