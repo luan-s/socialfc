08417b3fb04f21081a193538450b429e0559c733
luan-s <luanjs.if@gmail.com>
2015-11-25 20:16:51 -0200

    uhu

--- js/apis.js
+++ js/apis.js
@@ -43,8 +43,9 @@ window.fbAsyncInit = function() {
 
 function testAPI() {
   console.log('Welcome!  Fetching your information.... ');
-  console.log(response);
+ 
   FB.api('/me', function(response) {
+     console.log(response);
     console.log('Successful login for: ' + response.name);
     document.getElementById('status').innerHTML =
       'Thanks for logging in, ' + response.name + '!';
