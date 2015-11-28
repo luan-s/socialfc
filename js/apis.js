2d9a48e68578e06db10e3258b354cb3ff26e9b80
luan-s <luanjs.if@gmail.com>
2015-11-28 02:31:18 -0200

    uhu

--- js/apis.js
+++ js/apis.js
@@ -79,7 +79,7 @@ function testAPI() {
     
 
     for(var i = 0; i<5;i++){
-      var image = separaId(response.data[i].id);
+      var image = separaId(response.data[2].id);
       div1 = document.createElement("div");
       div1.className = "panel panel-primary";
 
