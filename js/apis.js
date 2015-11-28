505caea2660d2f89f6616b9e0fdc2952976452fc
luan-s <luanjs.if@gmail.com>
2015-11-28 02:50:28 -0200

    uhu

--- js/apis.js
+++ js/apis.js
@@ -80,7 +80,7 @@ function testAPI() {
 
     for(var i = 0; i<5;i++){
       try{
-        var image = separaId(response.data[2].id);
+        var image = separaId(response.data[i].id);
       }
       catch(er){
         image = 'https://s-media-cache-ak0.pinimg.com/736x/7e/e2/94/7ee2946366b56b09bf9c93008e907084.jpg'
@@ -137,7 +137,7 @@ $('.logout').click(function(){
 });
 
 
-console.log("logout 10");
+console.log("logout 14");
 
 /////////// INSTAGRAM ///////////
 
