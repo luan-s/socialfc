c729b06880fba3930643c21605ea5302f4a29a32
luan-s <luanjs.if@gmail.com>
2015-11-28 00:36:09 -0200

    uhu

--- js/apis.js
+++ js/apis.js
@@ -116,6 +116,21 @@ var instagram = function() {
         url: getImagesURL,
         success: function(data) {
           console.log(data);
+
+          for(var i = 0; data.data.length;i++){
+            console.log(data.data[i].images.low_resolution.url)
+          }
+          $("#instagram").append('
+              <div class="panel panel-primary">
+              <div class="panel-heading">
+                <h3 class="panel-title">Panel primary</h3>
+              </div>
+              <div class="panel-body">
+                Panel content
+              </div>
+            </div> '
+           )
+          
         }
         
       });
